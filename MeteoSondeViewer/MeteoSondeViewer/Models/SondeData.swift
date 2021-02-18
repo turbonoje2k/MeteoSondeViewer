8f08da59a5bcad0a37fe3df0c8aae15c017edf99
Noje <9hnoje@gmail.com>
2021-02-18 22:28:05 +0100

    add json parsing

---
+++ MeteoSondeViewer/MeteoSondeViewer/Models/SondeData.swift
@@ -0,0 +1,14 @@
+//
+//  SondeData.swift
+//  MeteoSondeViewer
+//
+//  Created by noje on 18/02/2021.
+//
+
+import Foundation
+
+struct SondeData {
+    var Temperatur: String = ""
+    var Humidity: String = ""
+    
+}
